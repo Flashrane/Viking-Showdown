9697b1d26bc0d4ec114e7779d503003d500a4464
Flashrane <szasz.csongor12@gmail.com>
2022-04-28 00:35:14 +0300

    Limit camera movement from overshooting - using a bounding box

--- Project/Assets/Scripts/CameraFollow.cs
+++ Project/Assets/Scripts/CameraFollow.cs
@@ -8,6 +8,7 @@ public class CameraFollow : MonoBehaviour
     public Vector3 offset;
     [Range(1,10)]
     public float smoothFactor;
+    public Vector3 minValues, maxValues;
 
     void FixedUpdate()
     {
@@ -17,7 +18,11 @@ public class CameraFollow : MonoBehaviour
     void Follow()
     {
         Vector3 targetPosition = target.position + offset;
-        Vector3 smoothPosition = Vector3.Lerp(transform.position, targetPosition, smoothFactor * Time.fixedDeltaTime); // the higher the smoothFactor the faster the transition
+        Vector3 boundPosition = new Vector3(
+            Mathf.Clamp(targetPosition.x, minValues.x, maxValues.x),
+            Mathf.Clamp(targetPosition.y, minValues.y, maxValues.y),
+            Mathf.Clamp(targetPosition.z, minValues.z, maxValues.z));
+        Vector3 smoothPosition = Vector3.Lerp(transform.position, boundPosition, smoothFactor * Time.fixedDeltaTime); // the higher the smoothFactor the faster the transition
         transform.position = smoothPosition;
     }
 }
