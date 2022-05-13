6920a5d0d25b44a2773de7be5cf07cec07ccb31b
Flashrane <szasz.csongor12@gmail.com>
2022-05-14 01:32:31 +0300

    Set up controls menu design

--- Project/Assets/Scripts/StartMenu.cs
+++ Project/Assets/Scripts/StartMenu.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class StartMenu : MonoBehaviour
 {
@@ -16,8 +19,36 @@ public class StartMenu : MonoBehaviour
         audio.Play("StartMenuMusic");
     }
 
+    void Start()
+    {
+        Invoke("EnableHoverSound", 2f);
+    }
+
+    void EnableHoverSound()
+    {
+        AudioSource[] sources = audio.GetComponents<AudioSource>();
+        foreach (AudioSource source in sources)
+        {
+            if (source.clip.name == "Hover")
+            {
+                source.volume = 1;
+                return;
+            }
+        }
+    }
+
     public void ExitGame()
     {
         Application.Quit();
+    }
+
+    public void Hover()
+    {
+        audio.Play("ButtonHover");
+    }
+
+    public void Select()
+    {
+        audio.Play("ButtonSelect");
     }
 }
