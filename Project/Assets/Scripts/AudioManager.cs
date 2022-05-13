d0f3a8b4dcdf28279fa6800a690b85d37ffeb1f0
Csongor Szász <szasz.csongor12@gmail.com>
2022-05-13 13:06:49 +0300

    Set up sounds in assets folder; try out the music in the start menu

---
+++ Project/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,17 @@
+using UnityEngine.Audio;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
